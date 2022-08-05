5827de3872ac32def2cddc47cbecbd9e85806603
tur1ng <mohammadali.ap.2000@gmail.com>
2022-08-06 04:01:31 +0430

    feat: add features blog

---
+++ content/blog/how-i-built-my-blog.md
@@ -0,0 +1,5 @@
+---
+title: How I Built My Blog using Fresh 
+desc: An in-depth look at the technical stack behind this very blog! We'll see how I use Fresh to serve and islands to add intractivity, how I use GFM to parse markdown, and how I organize my codebase, among others.
+---
+# Test 
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ import { Head } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 import Container from "../components/Container.tsx";
 import Navigation from "../components/Navigation.tsx";
-import { PageProps } from "$fresh/server.ts";
+import features from "../content/features.json" assert { type: "json" };
 
 const TITLE = "Mohammad ali Ali panah";
 const DESCRIPTION = "Mohammad ali Ali panah - Front-end lead at Zoomit";
@@ -23,6 +23,7 @@ export default function Home() {
         <Container>
           <Navigation active="/" />
           <Me />
+          <Features features={features.blog} />
         </Container>
       </main>
     </>
@@ -48,16 +49,76 @@ function Me() {
             Zoomit
           </a>
         </h2>
+        <p class={tw`text-gray-600 dark:text-gray-400 mb-16`}>
+          Collecting my learnings about software engineering, computer science,
+          web development, and system design.
+        </p>
       </div>
-      <div class={tw`w-[80px] sm:w-[121px] relative mb-8 sm:mb-0 mr-auto`}>
+      <div class={tw`w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 `}>
         <img
           alt="Mohammad ali Ali panah"
           src="/me.jpeg"
-          width={121}
-          height={121}
+          width={176}
+          height={176}
           class={tw`rounded-full`}
         />
       </div>
     </div>
+  );
+}
+
+type FeaturesProps = {
+  features: Feature[];
+};
+
+function Features(props: FeaturesProps) {
+  return (
+    <>
+      <h3
+        class={tw`font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white`}
+      >
+        Featured Blogs
+      </h3>
+      <div class={tw`flex gap-6 flex-col md:flex-row`}>
+        {props.features.map((feature) => (
+          <FeatureCard
+            key={feature.title}
+            title={feature.title}
+            link={feature.link}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+type Feature = {
+  title: string;
+  link: string;
+};
+
+function FeatureCard(props: Feature) {
+  return (
+    <a
+      href={props.link}
+      class={tw`transform hover:scale-[1.01] transition-all rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1 from-[#D8B4FE] to-[#818CF8]`}
+    >
+      <div
+        class={tw`flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4`}
+      >
+        <div class={tw`flex flex-col md:flex-row justify-between`}>
+          <h4
+            class={tw`text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full text-gray-900 dark:text-gray-100 tracking-tight`}
+          >
+            {props.title}
+          </h4>
+        </div>
+        <div
+          class={tw`flex items-center text-gray-800 dark:text-gray-200 capsize`}
+        >
+          ???
+        </div>
+      </div>
+    </a>
   );
 }
